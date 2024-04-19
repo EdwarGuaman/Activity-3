33509e5b55f948add966fca5702ce9541dae48d0
EdwarGuaman <edwar.guaman-95-01@outlook.es>
2024-04-19 09:09:06 -0500

    lista-pila-cola

---
+++ main.js
@@ -0,0 +1,133 @@
+//LISTA ENLAZADA
+class NodoLista{
+
+    constructor(valor){
+        this.valor=valor;
+        this.siguiente=null;
+    
+    }
+
+}
+let primero=null;
+
+function agregarElemento(valor){
+
+    let nuevo= new NodoLista(valor);
+    nuevo.siguiente=primero;
+    primero=nuevo;
+
+}
+function mostrar(){
+
+    let actual=primero;
+    while(actual!=null){
+
+        console.log(actual.valor);
+        actual=actual.siguiente;
+    }
+
+}
+
+function eliminar(){
+    primero=primero.siguiente;
+}
+
+
+
+agregarElemento(1);
+agregarElemento(2);
+agregarElemento(10);
+agregarElemento(20);
+mostrar();
+
+
+//PILAS
+class NodoPila{
+
+    constructor(valor){
+        this.valor=valor;
+        this.siguiente=null;
+    
+    }
+
+}
+
+let primeropila=null;
+
+class Pila{
+
+    constructor(){
+
+        this.tope=null;
+    }
+
+    agregarElementoPila(valor){
+
+        let nuevo=new NodoPila(valor);
+        nuevo.siguiente=this.tope;
+        this.tope=nuevo;
+        
+    }
+
+    mostrar(){
+
+        let actual=this.tope;
+        while(actual!=null){
+
+        console.log(actual.valor);
+        actual=actual.siguiente;
+    }
+
+
+    }
+
+
+}
+
+
+
+let mipila= new Pila();
+mipila.agregarElementoPila(15);
+mipila.agregarElementoPila(150);
+mipila.agregarElementoPila(1500);
+mipila.agregarElementoPila(15000);
+mipila.mostrar();
+
+console.log(mipila);
+
+
+
+
+//COLAS
+
+
+
+class NodoCola{
+
+    constructor(valor){
+        this.valor=valor;
+        this.siguiente=null;
+    
+    }
+
+}
+
+class Cola{
+
+    constructor(){
+
+        this.cola=[];
+
+    }
+
+    agregarElementoCola(valor){
+
+        this.cola.push(valor);
+    }
+
+    eliminarElementoCola(){
+
+        return this.cola.shift();
+    }
+
+}
