23c1907f5c4625a603cecb1eeee8316e0b01b394
EdwarGuaman <edwar.guaman-95-01@outlook.es>
2024-04-22 18:48:38 -0500

    ARBOL BINARIO

--- main.js
+++ main.js
@@ -1,133 +1,169 @@
-//LISTA ENLAZADA
-class NodoLista{
+//ARBOLES
+
+class Nodo{
 
     constructor(valor){
+
         this.valor=valor;
-        this.siguiente=null;
-    
-    }
-
-}
-let primero=null;
-
-function agregarElemento(valor){
-
-    let nuevo= new NodoLista(valor);
-    nuevo.siguiente=primero;
-    primero=nuevo;
-
-}
-function mostrar(){
-
-    let actual=primero;
-    while(actual!=null){
-
-        console.log(actual.valor);
-        actual=actual.siguiente;
-    }
-
-}
-
-function eliminar(){
-    primero=primero.siguiente;
-}
-
-
-
-agregarElemento(1);
-agregarElemento(2);
-agregarElemento(10);
-agregarElemento(20);
-mostrar();
-
-
-//PILAS
-class NodoPila{
-
-    constructor(valor){
-        this.valor=valor;
-        this.siguiente=null;
-    
-    }
-
-}
-
-let primeropila=null;
-
-class Pila{
-
-    constructor(){
-
-        this.tope=null;
-    }
-
-    agregarElementoPila(valor){
-
-        let nuevo=new NodoPila(valor);
-        nuevo.siguiente=this.tope;
-        this.tope=nuevo;
+        this.izquierda=null;
+        this.derecha=null;
         
     }
-
-    mostrar(){
-
-        let actual=this.tope;
-        while(actual!=null){
-
-        console.log(actual.valor);
-        actual=actual.siguiente;
-    }
-
-
-    }
-
-
 }
 
-
-
-let mipila= new Pila();
-mipila.agregarElementoPila(15);
-mipila.agregarElementoPila(150);
-mipila.agregarElementoPila(1500);
-mipila.agregarElementoPila(15000);
-mipila.mostrar();
-
-console.log(mipila);
-
-
-
-
-//COLAS
-
-
-
-class NodoCola{
-
-    constructor(valor){
-        this.valor=valor;
-        this.siguiente=null;
-    
-    }
-
-}
-
-class Cola{
+class arbolBinario{
 
     constructor(){
+        this.raiz=null;
+    }
 
-        this.cola=[];
+//  INICIO AGREGAR
+    agregarElemento(valor){
+        let nuevo=new Nodo(valor);
+        if(this.raiz==null){
+
+            this.raiz=nuevo;
+    
+        }else{
+
+            this.agregar(nuevo,this.raiz);
+
+        }
 
     }
 
-    agregarElementoCola(valor){
+    agregar(nuevo,nodo){
+        //comparacion izquierda
+        if(nuevo.valor<nodo.valor){ 
+           
+            if(nodo.izquierda==null){
 
-        this.cola.push(valor);
+                nodo.izquierda=nuevo;
+            }
+            else{
+                this.agregar(nuevo,nodo.izquierda);
+            }
+           
+        }else{
+
+            // comparacion derecha
+
+            if(nodo.derecha==null){
+
+                nodo.derecha=nuevo;
+
+            }
+            else{
+                this.agregar(nuevo,nodo.derecha);
+            }
+
+        }
+    }
+//FIN AGREGAR
+
+// BUSQUEDA DE VALOR
+
+    buscar(valor){
+
+        return this.buscarElemento(valor,this.raiz);
     }
 
-    eliminarElementoCola(){
+    buscarElemento(valor,nodo){
 
-        return this.cola.shift();
+        if(nodo!=null){
+
+            if(valor<nodo.valor){
+                return this.buscarElemento(valor,nodo.izquierda);
+            }
+            else{
+
+                if(valor>nodo.valor){
+                    return this.buscarElemento(valor,nodo.derecha);
+                }
+                else{
+                    return nodo;
+                }
+            }
+        }
     }
+//FIN BUSQUEDA VALOR
+
+//RECORRIDO EN ORDEN
+    buscarenOrden(){
+
+        let resultado=[];
+        this.enOrdenAux(this.raiz,resultado);
+        return resultado;
+    }
+
+    enOrdenAux(nodo, resultado){
+
+        if(nodo!=null){
+
+            this.enOrdenAux(nodo.izquierda,resultado);
+            resultado.push(nodo.valor);
+            this.enOrdenAux(nodo.derecha,resultado);
+            
+        }
+
+    }
+// FIN RECORRIDO ORDEN
+
+// RECORRIDO EN PREORDEN
+
+    buscarenPreOrden(){
+        let resultado=[];
+        this.enPreOrdenAux(this.raiz,resultado);
+        return resultado;
+    }
+
+
+    enPreOrdenAux(nodo,resultado){
+
+        if(nodo!=null){
+
+            resultado.push(nodo.valor);
+            this.enPreOrdenAux(nodo.izquierda,resultado);
+            this.enPreOrdenAux(nodo.derecha,resultado);
+        }
+    }
+
+// FIN RECORRIDO PREORDEN
+
+// RECORRIDO POSTORDEN
+
+    buscarenPostOrden(){
+        let resultado=[];
+        this.enPostOrdenAux(this.raiz,resultado);
+        return resultado;
+    }
+
+
+    enPostOrdenAux(nodo,resultado){
+
+        if(nodo!=null){
+
+            this.enPostOrdenAux(nodo.izquierda,resultado);
+            this.enPostOrdenAux(nodo.derecha,resultado);
+            resultado.push(nodo.valor);
+            
+            
+        }
+    }
+
+// FIN RECORRIDO POSTORDEN    
 
 }
+
+
+let myarbol= new arbolBinario();
+
+myarbol.agregarElemento(10);
+myarbol.agregarElemento(7);
+myarbol.agregarElemento(20);
+myarbol.agregarElemento(15);
+//console.log(myarbol);
+//console.log(myarbol.buscar(20));
+console.log(myarbol.buscarenOrden());
+console.log(myarbol.buscarenPreOrden());
+console.log(myarbol.buscarenPostOrden());
